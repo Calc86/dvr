0ad5364dab37a1fa9458dca6a95bd80bcdf52175
calc <calc@list.ru>
2014-03-28 18:52:56 +0400

    task #55 big-brother-55 сделать fork на prodution, devel и test

--- bin/startup/startup.php
+++ bin/startup/startup.php
@@ -9,13 +9,19 @@ $q = "select id,login from users where banned=0";
 $r = $db->query($q);
 if(!$r) throw new MysqlQueryException($q);
 
-while(($row = $r->fetch_row()) != 0){
-    list($uid,$name) = $row;
-    $vlc = new vlc(new UserID($uid));
-    echo "запускаем $name\n";
-    $vlc->ps_kill(); //убить живые процессы, если есть (для безопасности)
-    $vlc->start();
+try{
+    while(($row = $r->fetch_row()) != 0){
+        list($uid,$name) = $row;
+        $vlc = new vlc(new UserID($uid));
+        echo "запускаем $name\n";
+        $vlc->ps_kill(); //убить живые процессы, если есть (для безопасности)
+        $vlc->start();
+    }
 }
+catch (Exception $e){
+    throw $e;
+}
+
 
 $db->close();
 
