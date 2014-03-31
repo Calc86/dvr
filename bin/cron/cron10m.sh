8928d0cc3e3a590e76ebbbbc3fbab59bc43b3e7e
calc <calc@list.ru>
2014-03-31 09:39:37 +0400

    error with cron

--- bin/cron/cron10m.sh
+++ bin/cron/cron10m.sh
@@ -5,7 +5,8 @@ DIR=$VLCDIR
 BIN=$DIR/bin
 ETC=$DIR/etc
 
-php $BIN/control/restart_record.php
+#php $BIN/control/restart_record.php
+php $BIN/system/Main.php update
 
 # очистим ненужные записи
 php $BIN/util/rec-clear.php
@@ -16,9 +17,9 @@ find $DIR/rec -empty -mtime +1 -delete
 find $DIR/mtn -empty -mtime +1 -delete
 
 # ротация логов организаций
-for i in `find $ETC | grep logrotate.conf`
-do
-  /sbin/logrotate -s /dev/null $i >> /dev/null
-done
+#for i in `find $ETC | grep logrotate.conf`
+#do
+#  /sbin/logrotate -s /dev/null $i >> /dev/null
+#done
 
 php $BIN/util/rec-pts.php &
