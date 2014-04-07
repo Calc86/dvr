9125349b52b695cfdb091b22ff5260f3c9da8a68
calc <calc@list.ru>
2014-04-07 23:25:33 +0400

    initial motion with config and stop/start routine

---
+++ bin/system/motion/MotionCOnfig.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: calc
+ * Date: 07.04.14
+ * Time: 20:13
+ */
+
+namespace system;
+
+/**
+ * Class MotionConfig
+ * @package system
+ */
+class MotionConfig {
+
+    /**
+     * @param String $config текстовый конфиг из темплейта
+     * @param array $params какие значения поставить key(motion)=>value
+     * @return string
+     */
+    public static function parseConfig($config, array $params = array()){
+        /**
+         * Array
+        (
+            [0] => Array
+            (
+                [0] => {log_level:6}
+                [1] => log_level
+                [2] => 6
+            )
+         */
+        $pattern = "/[{]([a-z_]*)[:]?([a-z0-9]*)[}]/";
+        $matches = array();
+        preg_match_all($pattern, $config, $matches, PREG_SET_ORDER);
+        //подготовим масив с дефолными значениями
+        $settings = array();
+        foreach($matches as $match){
+            $settings['search'][] = $match[0];
+            $settings['replace'][] = isset($params[$match[1]]) ? $params[$match[1]] : $match[2];
+        }
+
+        return str_replace($settings['search'], $settings['replace'], $config);
+    }
+}
