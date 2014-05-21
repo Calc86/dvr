dfc65ef21d118e6416575be3011c1eae137e37c2
calc <calc@list.ru>
2014-05-21 16:58:07 +0400

    some includes

--- bin/system2/classes/include.php
+++ bin/system2/classes/include.php
@@ -8,9 +8,12 @@
 
 require_once __DIR__.'/common/include.php';
 
-require_once __DIR__.'/vlc/include.php';
+require_once __DIR__.'/lock/include.php';
 require_once __DIR__.'/log/include.php';
-require_once __DIR__.'/motion/include.php';
 require_once __DIR__.'/mysql/include.php';
+
+require_once __DIR__.'/vlc/include.php';
+require_once __DIR__.'/motion/include.php';
+
 require_once __DIR__.'/bb/include.php';
 require_once __DIR__.'/tv/include.php';
