51e891c3c80407f3ea299d48630bd829561b0dd6
calc <calc@list.ru>
2014-05-21 16:55:37 +0400

    TimeLock

---
+++ bin/system2/classes/lock/TimeLock.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: calc
+ * Date: 20.05.14
+ * Time: 15:23
+ */
+
+namespace system2;
+
+/**
+ * Нужно использовать create, если создается, то время прошло
+ * Class TimeLock
+ * @package system2
+ */
+class TimeLock extends Lock {
+
+    private $time;
+
+    /**
+     * @param string $fName
+     * @param int $time in seconds
+     */
+    function __construct($fName, $time = 600)
+    {
+        $this->time = $time;
+        parent::__construct('time_'.$fName);
+
+
+        if(file_exists($this->getPath())){
+            $sub = time() - filectime($this->getPath());
+            if( $sub > $time ){
+                $this->delete();
+            }
+            else{
+                Log::getInstance()->put(($time - $sub),"TimeLock");
+            }
+        }
+    }
+}
