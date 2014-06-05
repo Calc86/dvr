b5588b4a468097b03be2d9e240dff6b692573436
calc <calc@list.ru>
2014-06-05 15:16:51 +0400

    no TVUser, only factory

--- bin/system2/classes/tv/TVUser.php
+++ bin/system2/classes/tv/TVUser.php
@@ -12,9 +12,9 @@ namespace system2;
  * Class TVUser
  * @package system2
  */
-class TVUser extends User {
+/*class TVUser extends User {
     protected function _create()
     {
         $this->dvrs[] = AbstractFactory::getInstance()->createDvr($this);
     }
-}
+}*/
