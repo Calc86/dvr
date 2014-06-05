d762404a22da72f5ccd72795bc0c1692ac1529f6
calc <calc@list.ru>
2014-06-05 15:27:13 +0400

    factory

--- bin/system2/classes/tv/include.php
+++ bin/system2/classes/tv/include.php
@@ -8,8 +8,5 @@
 
 require_once __DIR__.'/TVHlsStream.php';
 require_once __DIR__.'/TVReStream.php';
-require_once __DIR__.'/TVCam.php';
-require_once __DIR__.'/TVDvr.php';
-require_once __DIR__.'/TVUser.php';
 require_once __DIR__.'/TVSystem.php';
 require_once __DIR__.'/TVFactory.php';
