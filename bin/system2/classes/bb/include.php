9e64b3f8c731dda142119fd86ef92e000bc995d1
calc <calc@list.ru>
2014-06-18 08:19:39 +0400

    fix

--- bin/system2/classes/bb/include.php
+++ bin/system2/classes/bb/include.php
@@ -7,10 +7,13 @@
  */
 
 require_once __DIR__.'/mysql.php';
+
+require_once __DIR__.'/BBFactory.php';
+
 require_once __DIR__.'/BBLiveStream.php';
 require_once __DIR__.'/BBCamSettings.php';
 require_once __DIR__.'/BBRecStream.php';
-require_once __DIR__.'/BBCam.php';
-require_once __DIR__.'/BBDvr.php';
-require_once __DIR__.'/BBUser.php';
+//require_once __DIR__.'/BBCam.php';
+//require_once __DIR__.'/BBDvr.php';
+//require_once __DIR__.'/BBUser.php';
 require_once __DIR__.'/BBSystem.php';
