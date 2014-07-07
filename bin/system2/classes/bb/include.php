f4afd9b375b3b661e62d05e656c0f8c155d88cdf
calc <calc@list.ru>
2014-07-07 12:28:17 +0400

    includes

--- bin/system2/classes/bb/include.php
+++ bin/system2/classes/bb/include.php
@@ -7,11 +7,14 @@
  */
 
 require_once __DIR__.'/mysql.php';
+require_once __DIR__.'/Events.php';
 
 require_once __DIR__.'/BBLogMotionEvent.php';
 require_once __DIR__.'/BBRecMotionEvent.php';
 require_once __DIR__.'/RotateRecCommand.php';
 require_once __DIR__.'/BBArchiveTimelapseCommand.php';
+require_once __DIR__.'/BBTestInputFailSaveCommand.php';
+require_once __DIR__.'/BBDaemonWatchdog.php';
 
 
 require_once __DIR__.'/BBFactory.php';
