cd9b9620c495f52a527c788fadb1e97d361169b2
Gorbachev Andrey <calc@list.ru>
2021-10-23 01:15:47 +0300

    [*]

---
+++ htdocs/basic/modules/vlc/components/application/bin/system2/classes/common/BashCommand2.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: calc
+ * Date: 24.06.14
+ * Time: 10:48
+ */
+
+namespace system2;
+
+
+use app\modules\vlc\components\ICommand;
+
+/**
+ *
+ */
+class BashCommand2 implements ICommand {
+    private string $cmd;
+
+    /**
+     * @param string $cmd
+     */
+    function __construct(string $cmd)
+    {
+        $this->cmd = $cmd;
+    }
+
+
+    /**
+     * @return void
+     */
+    public function execute()
+    {
+        $bash = new BashCommand2($this->cmd);
+        $bash->execute();
+    }
+}
