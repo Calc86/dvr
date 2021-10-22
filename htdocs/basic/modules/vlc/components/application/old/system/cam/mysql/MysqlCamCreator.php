9ea0baa4ef5177b16a6ed3218cb860bcb477bcbc
Gorbachev Andrey <calc@list.ru>
2021-10-23 01:07:33 +0300

    [*]

--- htdocs/basic/modules/vlc/components/application/old/system/cam/mysql/MysqlCamCreator.php
+++ htdocs/basic/modules/vlc/components/application/old/system/cam/mysql/MysqlCamCreator.php
@@ -8,9 +8,14 @@
 
 namespace system;
 
+use app\modules\vlc\components\Database;
+use app\modules\vlc\components\exceptions\MysqlQueryException;
+use app\modules\vlc\types\CamID;
+use app\modules\vlc\types\UserID;
+
 /**
  * Class MysqlCamCreator
- * Итератор для создания камер из Mysql
+ * Итератор для создания камер from Mysql
  * @package system
  */
 class MysqlCamCreator extends CamCreator {
@@ -18,13 +23,14 @@ class MysqlCamCreator extends CamCreator {
     private $count = 0;
 
     /**
-     * @param \UserID $dvr_id
+     * @param UserID $dvr_id
+     * @throws MysqlQueryException
      */
-    function __construct(\UserID $dvr_id)
+    function __construct(UserID $dvr_id)
     {
         parent::__construct($dvr_id);
 
-        $db = \Database::getInstance();
+        $db = Database::getInstance();
 
         $q = "select id, ip, live, rec, mtn from cams where user_id = $this->dvr_id";
         $r = $db->query($q);
@@ -33,7 +39,7 @@ class MysqlCamCreator extends CamCreator {
         while(($row = $r->fetch_object('system\MysqlCam',array($this->dvr_id))) != null){
             /** @var Cam $row */
             //залить параметры
-            $camMotion = new MysqlCamMotion($this->dvr_id, new \CamID($row->getID()), $row->getIp());
+            $camMotion = new MysqlCamMotion($this->dvr_id, new CamID($row->getID()), $row->getIp());
             $row->setCamMotion($camMotion);
 
             $this->cams[$row->getMysqlId()] = $row;
